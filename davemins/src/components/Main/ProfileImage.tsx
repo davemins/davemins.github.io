d93020eff999937ef000b95939c84b3cd0986c0c
davemins <davemins4125@naver.com>
2024-08-28 18:22:57 +0900

    feat : 메인페이지 만들기

---
+++ davemins/src/components/Main/ProfileImage.tsx
@@ -0,0 +1,20 @@
+import React, { FunctionComponent } from 'react'
+import styled from '@emotion/styled'
+
+// 자신이 원하는 프로필 이미지 링크로 설정해주세요.
+const PROFILE_IMAGE_LINK =
+    'https://github.com/user-attachments/assets/cfbaf740-38e4-4a85-ba45-c02dad3a149e'
+
+
+const ProfileImageWrapper = styled.img`
+    width: 120px;
+    height: 120px;
+    margin-bottom: 30px;
+    border-radius: 50%;
+`
+
+const ProfileImage: FunctionComponent = function () {
+    return <ProfileImageWrapper src={PROFILE_IMAGE_LINK} alt="Profile Image" />
+}
+
+export default ProfileImage
