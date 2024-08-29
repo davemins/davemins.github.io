7ca26ba7de0e9751d7c6cf6d73de36d9cbc823a8
davemins <davemins4125@naver.com>
2024-08-29 10:10:36 +0900

    feat : Props로 포스트 데이터 출력 test

--- davemins/src/components/Main/PostList.tsx
+++ davemins/src/components/Main/PostList.tsx
@@ -2,15 +2,23 @@ import React, { FunctionComponent } from 'react'
 import styled from '@emotion/styled'
 import PostItem from 'components/Main/PostItem'
 
-const POST_ITEM_DATA = {
-    title: 'Post Item Title',
-    date: '2020.01.29.',
-    categories: ['Web', 'Frontend', 'Testing'],
-    summary:
-        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident repellat doloremque fugit quis rem temporibus! Maxime molestias, suntrem debitis odit harum impedit. Modi cupiditate harum dignissimos eos in corrupti!',
-    thumbnail:
-        '<https://ji5485.github.io/static/e4f34c558ae8e8235ff53b0311085796/4d854/javascript-core-concept-summary-function-1.webp>',
-    link: '<https://www.google.co.kr/>',
+export type PostType = {
+    node: {
+        id: string
+        frontmatter: {
+            title: string
+            summary: string
+            date: string
+            categories: string[]
+            thumbnail: {
+                publicURL: string
+            }
+        }
+    }
+}
+
+type PostListProps = {
+    posts: PostType[]
 }
 
 const PostListWrapper = styled.div`
@@ -28,15 +36,22 @@ const PostListWrapper = styled.div`
     }
 `
 
-const PostList: FunctionComponent = function () {
+const PostList: FunctionComponent<PostListProps> = function ({ posts }) {
     return (
-        <PostListWrapper>
-            <PostItem {...POST_ITEM_DATA} />
-            <PostItem {...POST_ITEM_DATA} />
-            <PostItem {...POST_ITEM_DATA} />
-            <PostItem {...POST_ITEM_DATA} />
-        </PostListWrapper>
+      <PostListWrapper>
+        {posts.map(
+          ({
+            node: { id, frontmatter },
+          }: PostType) => (
+            <PostItem
+              {...frontmatter}
+              link="https://www.google.co.kr/"
+              key={id}
+            />
+          ),
+        )}
+      </PostListWrapper>
     )
-}
-
-export default PostList
+  }
+  
+  export default PostList
